5c706022f0a84d3d2161306d726120db9a1443e4
Bastian Garcia <bastian.garcia@bicevida.cl>
2023-08-18 19:41:33 -0400

    test

---
+++ main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import redis
+
+app = FastAPI()
+
+# A      adiendo middleware para CORS
+origins = [
+    "*"
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Conexi      n a Redis
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+@app.post("/set_hash/")
+async def set_hash(data: dict):
+    key_value = data.pop("key", None)
+    print(data)
+    if not key_value:
+        raise HTTPException(status_code=400, detail="The 'key' is required in the JSON body.")
+
+    if not data:
+        raise HTTPException(status_code=400, detail="The body should have more than just the 'key'.")
+
+    redis_client.hmset(key_value, data)
+
+    return {"message": "Data saved successfully!"}
+
+@app.post("/get_hash/")
+async def get_hash(data: dict):
+    key_value = data.get("key")
+
+    if not key_value:
+        raise HTTPException(status_code=400, detail="The 'key' is required in the JSON body.")
+
+    result = redis_client.hgetall(key_value)
+
+    if not result:
+        raise HTTPException(status_code=404, detail=f"No hash found for key: {key_value}")
+
+    # Convert bytes to string for the result
+    result_str = {k.decode('utf-8'): v.decode('utf-8') for k, v in result.items()}
+
+    return result_str
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
